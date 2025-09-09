4b0b53e4eb671bc84991b1affcf8cb0982fa5acf
Toni Dobrinski <tdobrinski@abv.bg>
2025-09-09 23:07:09 +0300

    updated package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./App.css";
-import ExpenseItem from "./components/Expenses/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
